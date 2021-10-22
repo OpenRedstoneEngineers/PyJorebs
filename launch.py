ff1345e23d30d5fb93a2be900c9cb16efd1dac58
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2021-10-23 01:58:03 +0300

    Add server launcher

---
+++ launch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+
+image_name = "docker.io/library/openjdk:16.0.1-slim"
+
+def mounts_to_args(*mounts):
+    for (a, b) in mounts:
+        yield "-v"
+        yield f"{a}:{b}"
+
+def main(server, unit_name, runtime_dir):
+    mounts = mounts_to_args(
+        [f"/home/mcadmin/prod/{server}", "/data"],
+        ["/home/mcadmin/prod/paper", "/common"],
+        [f"/store/tiles/{server}", "/data/plugins/dynmap/web/tiles"],
+    )
+    # java opts?
+    paper_start_command = ["sh", "-c", '"cd /data && exec java -jar /common/paper.jar"']
+    podman_command = [
+        "podman", "run",
+        "--conmon-pidfile", f"{runtime_dir}/{unit_name}-pid",
+        "--cidfile", f"{runtime_dir}/{unit_name}-cid",
+        "--cgroups", "no-conmon",
+        "--name", f"mcserver-{server}",
+        *mounts,
+        # pod?
+        "--rm", "-dit",
+        image_name,
+        *paper_start_command,
+    ]
+    print(" ".join(podman_command))
+    
+if __name__ == "__main__":
+    main(*sys.arvg[1:])
